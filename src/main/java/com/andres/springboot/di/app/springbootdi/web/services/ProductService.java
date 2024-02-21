5a734f56f6ac88b0d175b9d4504ddb9c1f71fdfc
Jota Santos <jailsonsantosdpge@outlook.com>
2024-02-21 16:22:16 -0400

    Escribiendo la clase Service

--- src/main/java/com/andres/springboot/di/app/springbootdi/web/services/ProductService.java
+++ src/main/java/com/andres/springboot/di/app/springbootdi/web/services/ProductService.java
@@ -1,4 +1,20 @@
 package com.andres.springboot.di.app.springbootdi.web.services;
 
+import com.andres.springboot.di.app.springbootdi.core.entities.Product;
+import com.andres.springboot.di.app.springbootdi.infra.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private ProductRepository repository = new ProductRepository();
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id);
+    }
 }
