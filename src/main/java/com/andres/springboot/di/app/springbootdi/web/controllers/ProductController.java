fd6b147766095f148594c43dbf3826780a76cf5a
Jota Santos <jailsonsantosdpge@outlook.com>
2024-02-21 16:59:36 -0400

    Controller
    - Métodos list e show em ProductController

--- src/main/java/com/andres/springboot/di/app/springbootdi/web/controllers/ProductController.java
+++ src/main/java/com/andres/springboot/di/app/springbootdi/web/controllers/ProductController.java
@@ -1,4 +1,29 @@
 package com.andres.springboot.di.app.springbootdi.web.controllers;
 
+import com.andres.springboot.di.app.springbootdi.core.config.ApiPaths;
+import com.andres.springboot.di.app.springbootdi.core.entities.Product;
+import com.andres.springboot.di.app.springbootdi.web.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(ApiPaths.PRODUCT)
 public class ProductController {
+
+    private ProductService service = new ProductService();
+
+    @GetMapping
+    public List<Product> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
 }
