4aba8c266426f780e37605e33d848158e656aa10
jota santos <gestald118@email.com>
2024-03-11 22:08:06 -0400

    Creando la clase Controller

--- src/main/java/com/andres/springboot/di/app/springbootdi/controller/ProductController.java
+++ src/main/java/com/andres/springboot/di/app/springbootdi/controller/ProductController.java
@@ -1,4 +1,29 @@
 package com.andres.springboot.di.app.springbootdi.controller;
 
+import com.andres.springboot.di.app.springbootdi.config.ApiPath;
+import com.andres.springboot.di.app.springbootdi.entities.Product;
+import com.andres.springboot.di.app.springbootdi.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(ApiPath.PRODUCT)
 public class ProductController {
+
+    private ProductService productService = new ProductService();
+
+    @GetMapping
+    public List<Product> list() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
 }
