7938913ed33f0ce99f2d8a4057284d24ca705b16
Jota Santos <jailsonsantosdpge@outlook.com>
2024-04-02 18:00:16 -0400

    commit de segurnaça

---
+++ src/test/java/com/andres/curso/springboot/di/factura/springbootdi/SpringbootDifacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.curso.springboot.di.factura.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDifacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
